005f8c7df788c22fc7b521d097a0fa91f13ded9d
RiriZhepeton <118792487+RiriZhepeton@users.noreply.github.com>
2023-02-15 21:24:05 +0900

    Change ReportActivity code and xml

--- app/src/main/java/com/project/haruman/PostListActivity.java
+++ app/src/main/java/com/project/haruman/PostListActivity.java
@@ -12,4 +12,9 @@ public class PostListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_list);
     }
+
+    public void OnClickHandler_Button(View view){
+        Intent intent = new Intent(MainActivity.this, RequestActivity.class);
+        startActivity(intent);
+    }
 }
