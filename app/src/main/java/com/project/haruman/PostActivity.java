eab794242c767b678feba5dfac605a6abc68a496
Yeun1104 <93237371+Yeun1104@users.noreply.github.com>
2023-02-16 15:13:02 +0900

    Post and Setting Activity change

--- app/src/main/java/com/project/haruman/PostActivity.java
+++ app/src/main/java/com/project/haruman/PostActivity.java
@@ -2,7 +2,10 @@ package com.project.haruman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class PostActivity extends AppCompatActivity {
@@ -11,5 +14,24 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        Button mainbtn = findViewById(R.id.postActivity_button_back);
+        Button chatbtn = findViewById(R.id.postActivity_button_chat);
+
+        chatbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PostActivity.this,ChattingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mainbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PostActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/project/haruman/SettingActivity.java
+++ app/src/main/java/com/project/haruman/SettingActivity.java
@@ -2,7 +2,11 @@ package com.project.haruman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SettingActivity extends AppCompatActivity {
@@ -11,5 +15,25 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        Button signupbtn = findViewById(R.id.settingActivity_button_signup);
+
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button alarmbtn = findViewById(R.id.settingActivity_imageButton_alarm);
+        alarmbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+                startActivity(intent);
+            }
+        });
+
     }
 }
