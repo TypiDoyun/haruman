b909a3bdda2aecef78705e04243ee008218bd718
RiriZhepeton <118792487+RiriZhepeton@users.noreply.github.com>
2023-02-23 12:56:12 +0900

    Update RequestActivity.java

--- app/src/main/java/com/project/haruman/activities/RequestActivity.java
+++ app/src/main/java/com/project/haruman/activities/RequestActivity.java
@@ -15,6 +15,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.project.haruman.R;
 
 import java.sql.Time;
@@ -175,6 +181,28 @@ public class RequestActivity extends AppCompatActivity {
 
             }
         });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://127.0.0.1:3000/requestCreatePost";
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(RequestActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RequestActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        queue.add(stringRequest);
     }
 
     //xml Id를 각각 변수에 집어넣는 함수..
